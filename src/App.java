780e7fb9a962fcb3995ba8861ab509bf39a12e2a
VitorCaus <vitor50033@outlook.com>
2023-08-20 19:32:09 -0300

    Novos métodos adicionados ao Repository, testes adicionados no método main

--- src/App.java
+++ src/App.java
@@ -6,7 +6,7 @@ public class App {
         System.out.println("\n\nConsultando registros com Temperatura Maxima acima de 33 em 2015\n");
         consultas.pesquisa(reg->reg.getTempMaxima() > 33.0 && reg.getAno() == 2015).forEach(System.out::println);
         
-        System.out.println("Consultando datas registradas do ano 2000\n");
+        System.out.println("\n\nConsultando datas registradas do ano 2000\n");
         consultas.diasEmQue().forEach(System.out::println);
 
         System.out.println("\n\nConsultando datas com Temperatura Media acima de 30\n");
--- src/IConsultasRepository.java
+++ src/IConsultasRepository.java
@@ -4,6 +4,7 @@ import java.util.function.Predicate;
 
 interface IConsultasRepository {
     public Collection<RegistroDoTempo> todosDados();
+    public void cadastra(RegistroDoTempo novoRegistro);
     public RegistroDoTempo recupera(int dia, int mes, int ano);
     public List<RegistroDoTempo> pesquisa(Predicate<RegistroDoTempo> condicao);
     public boolean existe(int dia, int mes, int ano);
